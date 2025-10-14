d8a29ea8976ad30f28a8373c1b23bbc0314667ca
aashikur <mdaashikur@gmail.com>
2025-10-14 20:54:04 +0600

    arrow

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import { img } from "./assets/assets";
+import { IoMdArrowDown } from "react-icons/io";
 
 import Navbar from "./components/Navbar";
 import LinkSection from "./components/LinkSection";
 import Profile from "./components/Profile";
+import { FaArrowAltCircleDown } from "react-icons/fa";
 
 
 export default function Home() {
@@ -13,17 +15,26 @@ export default function Home() {
       <div className="flex flex-col   justify-center">
         <div className="text-center">
           {/* Profile Image */}
-          <Profile/>
+          <Profile />
 
           {/* Link Sections */}
           <LinkSection />
         </div>
 
+        <div className="mt-10 text-center">
+          <p className="text-gray-600 text-sm">Explore other side of Hridoy</p>
+          <button className="border border-gray-500 rounded-md p-2 mt-2 cursor-pointer group hover:text-gray-700 hover:bg-gray-200 transition">
+            <IoMdArrowDown className="text-[1.5rem] transform -rotate-135 transition-transform duration-300 group-hover:-translate-y-1 group-hover:translate-x-1" />
+          </button>
+
+
+
+        </div>
 
       </div>
 
 
-      <footer className="text-center py-4 border-t border-gray-700">
+      <footer className="text-center py-4 border-t border-gray-700 mt-10">
         <p className="text-sm text-gray-400">&copy; 2025 Joynal Abedin Hridoy. All rights reserved.</p>
       </footer>
 
